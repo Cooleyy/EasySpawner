30b932058a342a824a2cfbd6e6746f21a38ac812
Jason <jcool841@yahoo.com>
2021-03-01 13:55:50 +0000

    Name plugin and update gitignore

---
+++ EasySpawner/EasySpawner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using BepInEx;
+using HarmonyLib;
+
+namespace EasySpawner
+{
+    [BepInPlugin("cooley.easyspawner", "Easy Spawner", "1.0.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class EasySpawner : BaseUnityPlugin
+    {
+
+    }
+}
